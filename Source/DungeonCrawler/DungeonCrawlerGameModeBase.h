16aa94b45a42713639adf0126cd9fcefde8dbe00
Snorri Sturluson <snorri@arctictheory.com>
2021-04-02 12:50:53 +0000

    Initial commit

---
+++ Source/DungeonCrawler/DungeonCrawlerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DungeonCrawlerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DUNGEONCRAWLER_API ADungeonCrawlerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
